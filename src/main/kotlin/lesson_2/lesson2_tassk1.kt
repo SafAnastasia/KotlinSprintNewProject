f4f2b9c164438fb188858948ec87dc5b5289a3d3
Skarler <black2244@inbox.ru>
2025-02-15 19:03:25 +0300

    KSNP-2-1 the arithmetic mean was calculated

---
+++ src/main/kotlin/lesson_2/lesson2_tassk1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    val schBoy1: Int = 3
+    val schBoy2: Int = 4
+    val schBoy3: Int = 3
+    val schBoy4: Int = 5
+    val arithmeticMean = (schBoy1 + schBoy2 + schBoy3 + schBoy4) /4.0
+    println(arithmeticMean)
+}
