1ef2d590954a83eb7432f694292af6f70790b892
Skarler <black2244@inbox.ru>
2025-02-16 18:07:37 +0300

    KSNP-3-2 change of surname and age

---
+++ src/main/kotlin/lesson_3/lesson3_tassk2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_3
+
+fun main() {
+    var surname = "Андреева"
+    var age = "20"
+    println("$surname Татьяна Сергеевна, $age")
+    surname = "Сидорова"
+    age = "22"
+    println("$surname Татьяна Сергеевна, $age")
+
+}
