934b2a94445a573bd98b6e3ffef937557afeb94a
Skarler <black2244@inbox.ru>
2025-02-17 14:49:23 +0300

    KSNP-1-2 declared and overwrote variables

---
+++ src/main/kotlin/lesson_1/lesson1_tassk2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val quantityOrders: Int = 75
+    val thanksForThePurchase: String = "Благодарим за покупку"
+    println(quantityOrders)
+    println(thanksForThePurchase)
+    val totalNumberOfEmployees: Int = 2000
+    //println(totalNumberOfEmployees)
+    val totalNumberOfEmployees: Int = 1999
+    println(totalNumberOfEmployees)
+}
