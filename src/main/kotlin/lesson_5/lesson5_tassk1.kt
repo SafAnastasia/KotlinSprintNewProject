6126099da410a98f7f5c6325c7c4814dcab6778a
Skarler <black2244@inbox.ru>
2025-03-04 11:32:58 +0300

    KS-5-1-user authorization, addition of two numbers

---
+++ src/main/kotlin/lesson_5/lesson5_tassk1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_5
+
+fun main(){
+    println("Сколько будет 2+2?")
+    val answer = readln().toInt()
+    if (answer == 4){
+        println("Добро пожаловать!")
+    } else
+        println("Доступ запрещен")
+}
