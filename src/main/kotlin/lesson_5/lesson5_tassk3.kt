37ce6880363df36011870c1cbf4cdec75f7f7b32
Skarler <black2244@inbox.ru>
2025-03-05 16:07:55 +0300

    KS-5-3 spill-lottery, used if else construction

--- src/main/kotlin/lesson_5/lesson5_tassk3.kt
+++ src/main/kotlin/lesson_5/lesson5_tassk3.kt
@@ -4,13 +4,18 @@ const val FIRST_NUMBER = 0
 const val SECOND_NUMBER = 42
 
 fun main(){
-    println("Введите число: ")
-    val givenNumbers = readln().toInt()
-    if (givenNumbers == FIRST_NUMBER && givenNumbers == SECOND_NUMBER) {
+    println("Введите первое число: ")
+    val number1 = readln().toInt()
+    println("Введите второе число: ")
+    val number2 = readln().toInt()
+    if ((number1 == FIRST_NUMBER && number2 == SECOND_NUMBER) || (number1 == SECOND_NUMBER && number2 == FIRST_NUMBER))
+    {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if (givenNumbers == FIRST_NUMBER  || givenNumbers == SECOND_NUMBER) {
+    } else if (number1 == FIRST_NUMBER  ||  number1 == SECOND_NUMBER)  {
         println("Вы выиграли утешительный приз!")
-    } else {
+    } else if (number2 == FIRST_NUMBER  ||  number2 == SECOND_NUMBER)  {
+        println("Вы выиграли утешительный приз!")
+    }else {
         println("Неудача!")
     }
 
