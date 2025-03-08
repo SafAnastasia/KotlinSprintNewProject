13267899d874880329c017f57a9173721f26c697
Skarler <black2244@inbox.ru>
2025-03-08 14:26:23 +0300

    KS-4-3 edits have been made

--- src/main/kotlin/lesson_4/lesson4_tassk3.kt
+++ src/main/kotlin/lesson_4/lesson4_tassk3.kt
@@ -2,11 +2,11 @@ package org.example.lesson_4
 
 const val HUMIDITY = 20
 const val SEASON = "зима"
+const val IS_SUNNY = true
+const val TENT_OPEN = true
 
 fun main() {
-    val isSunny: Boolean = true
-    val tentOpen: Boolean = true
-    val favorableConditions = isSunny && tentOpen && HUMIDITY == 20 && SEASON != "зима"
+    val favorableConditions = IS_SUNNY && TENT_OPEN && HUMIDITY == 20 && SEASON != "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
