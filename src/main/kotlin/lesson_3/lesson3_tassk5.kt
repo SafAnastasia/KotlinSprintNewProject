4b1c0410f20d47c055e2d276a189669458cc69df
Skarler <black2244@inbox.ru>
2025-04-11 17:57:33 +0300

    KS-3-5 edits have been made

--- src/main/kotlin/lesson_3/lesson3_tassk5.kt
+++ src/main/kotlin/lesson_3/lesson3_tassk5.kt
@@ -2,18 +2,15 @@ package org.example.lesson_3
 
 fun main() {
     var playerString = "D2-D4;0"
-    var parts = playerString.split(";")
+    var parts = playerString.split(";","-")
     var part1 = parts[0]
     var part2 = parts[1]
-
-    var parts1 = part1.split("-")
-    var part3 = parts1[0]
-    var part4 = parts[1]
-
-    println("$part3\n" +
-            "$part4\n" +
-            "$part2")
-
+    var part3 = parts[2]
+    println(
+        "$part1\n" +
+                "$part2\n" +
+                "$part3"
+    )
 
 
 }
