2ff724b03f220d70df69ce623b607879a9531a1e
Skarler <black2244@inbox.ru>
2025-04-13 11:04:00 +0300

    KS-7-3 edits have been made

--- src/main/kotlin/lesson_7/lesson7_tassk3.kt
+++ src/main/kotlin/lesson_7/lesson7_tassk3.kt
@@ -1,17 +1,9 @@
 package org.example.lesson_7
 
 fun main() {
-
     println("Введите число: ")
-    val number = readLine()
-    val number1 = StringBuilder()
-
-    for (i in 0..number!!.toInt()) {
-        if (i % 2 == 0) {
-            number1.append("$i")
-            println("Четное число: $i")
-
-        }
-
+    val number = readLine().toInt()
+    for (i in 0..number step 2) {
+        println("Четное число: $i")
     }
 }
