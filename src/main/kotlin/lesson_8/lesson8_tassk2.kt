d0eca5ca4a3a100ac3939060554af2cc28c867c1
Skarler <black2244@inbox.ru>
2025-04-14 12:51:15 +0300

    KS-8-2 checking an ingredient in a recipe

---
+++ src/main/kotlin/lesson_8/lesson8_tassk2.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_8
+
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main() {
+    val pancakeRecipe = arrayOf("яйца", "молоко", "соль", "мука", "дрожжи")
+    println("Какой ингредиент нужно найти в рецепте: ")
+    val ingredient = readLine()
+    var found = false
+    for (i in pancakeRecipe) {
+        if (ingredient == i) {
+            found = true
+            break
+        }
+    }
+        if (found) {
+            println("Ингредиент $ingredient в рецепте есть")
+
+        } else {
+            println("Такого ингредиента в рецепте нет")
+
+        }
+    }
