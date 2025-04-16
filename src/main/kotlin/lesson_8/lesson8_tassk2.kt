bcb39dbd1b7816b63ccb8d98c7c7dca6e079fb35
Skarler <black2244@inbox.ru>
2025-04-16 16:51:30 +0300

    KS-8-2 edits made

--- src/main/kotlin/lesson_8/lesson8_tassk2.kt
+++ src/main/kotlin/lesson_8/lesson8_tassk2.kt
@@ -1,7 +1,5 @@
 package org.example.lesson_8
 
-import java.security.KeyStore.TrustedCertificateEntry
-
 fun main() {
     val pancakeRecipe = arrayOf("яйца", "молоко", "соль", "мука", "дрожжи")
     println("Какой ингредиент нужно найти в рецепте: ")
