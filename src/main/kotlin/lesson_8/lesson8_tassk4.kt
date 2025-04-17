bd32c19d4018f02651220188e0055560897e5866
Skarler <black2244@inbox.ru>
2025-04-17 14:49:40 +0300

    KS-8-4 edits have been made

--- src/main/kotlin/lesson_8/lesson8_tassk4.kt
+++ src/main/kotlin/lesson_8/lesson8_tassk4.kt
@@ -1,17 +1,17 @@
 package org.example.lesson_8
 
-fun main(){
+fun main() {
     val ingredients = arrayOf("морковь", "салат", "редис", "огурец")
     println("Какой ингредиент хотели ли бы заменить? ")
-    val ingredient = readLine()!!
+    val ingredient = readln()
     println("Введите ингредиент на который нужно заменить: ")
-    val ingredient2 = readLine()!!
-    val ingredient3 = ingredients.indexOf(ingredient)
+    val ingredient2 = readln()
 
     if (ingredient !in ingredients) {
         println("Этого ингредиента нет в списке")
     } else {
-        ingredients[ingredient3] = ingredient2
+        val index = ingredients.indexOf(ingredient)
+        ingredients[index] = ingredient2
         println("Готово! Вы сохранили следующий список: ${ingredients.joinToString()}")
     }
 }
