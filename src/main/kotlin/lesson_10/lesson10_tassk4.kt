3ac95efb8e82ea7821e266a55e2f0e50276b46fe
Skarler <black2244@inbox.ru>
2025-06-04 21:44:57 +0300

    KS-10-4 edits have been made

--- src/main/kotlin/lesson_10/lesson10_tassk4.kt
+++ src/main/kotlin/lesson_10/lesson10_tassk4.kt
@@ -15,26 +15,32 @@ fun main() {
         }
     }
 
-    println("Игра окончена. Вы выиграли $userWins партий.")
-}
-
-fun round() {
-    println("Ход игрока")
-    val manThrow = throwDice()
-    println("Игрок выбросил: $manThrow")
-
-    println("Ход компьютера")
-    val computerThrow = throwDice()
-    println("Компьютер выбросил: $computerThrow")
-
-    if (manThrow == computerThrow) {
-        println("Победила дружба")
+        println("Игра окончена. Вы выиграли $userWins партий.")
     }
-    if (manThrow > computerThrow) {
-        println("Победило человечество")
-    } else {
-        println("Победила машина")
+
+    fun round(): Boolean {
+        println("Ход игрока")
+        val manThrow = throwDice()
+        println("Игрок выбросил: $manThrow")
+
+        println("Ход компьютера")
+        val computerThrow = throwDice()
+        println("Компьютер выбросил: $computerThrow")
+
+        if (manThrow == computerThrow) {
+            println("Победила дружба")
+        }
+        if (manThrow > computerThrow) {
+            println("Победило человечество")
+        } else {
+            println("Победила машина")
+        }
     }
-}
+
+    fun throwDice(): Int {
+        return (1..6).random()
+    }
+
+
 
 
