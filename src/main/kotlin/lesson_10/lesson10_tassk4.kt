45c774dc7407fd6bb3beba66cc6046b8410964ce
Skarler <black2244@inbox.ru>
2025-06-05 17:09:49 +0300

    KS-10-4 edits have been made

--- src/main/kotlin/lesson_10/lesson10_tassk4.kt
+++ src/main/kotlin/lesson_10/lesson10_tassk4.kt
@@ -2,22 +2,17 @@ package org.example.lesson_10
 
 fun main() {
     var userWins = 0
-
     while (true) {
         val didUserWin = round()
         if (didUserWin) userWins++
-
         println("Хотите бросить кости еще раз? Введите Да или Нет:")
         val answer = readln()
-
         if (answer != "да") {
             break
         }
     }
-
         println("Игра окончена. Вы выиграли $userWins партий.")
     }
-
     fun round(): Boolean {
         println("Ход игрока")
         val manThrow = throwDice()
@@ -27,16 +22,21 @@ fun main() {
         val computerThrow = throwDice()
         println("Компьютер выбросил: $computerThrow")
 
-        if (manThrow == computerThrow) {
-            println("Победила дружба")
-        }
-        if (manThrow > computerThrow) {
+        return when {
+            manThrow == computerThrow -> {
+                println("Победила дружба")
+                false
+            }
+        manThrow > computerThrow -> {
             println("Победило человечество")
-        } else {
-            println("Победила машина")
+            true
+        }
+            else -> {
+                println("Победила машина")
+                false
+            }
         }
     }
-
     fun throwDice(): Int {
         return (1..6).random()
     }
