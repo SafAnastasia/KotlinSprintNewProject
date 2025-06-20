e8d7b97fe3589ebe604eeeb3719fb97e259a860c
Skarler <black2244@inbox.ru>
2025-06-20 13:12:10 +0300

    KS-11-2 additions have been made

--- src/main/kotlin/lesson_11/User.kt
+++ src/main/kotlin/lesson_11/User.kt
@@ -16,7 +16,43 @@ fun userData() {
 val user1 = User(123456, "kitis", "2G3K4L", "kitis32@mail.ru" )
 val user2 = User(67892, "cova", "9H8K4M", "sova22@mail.ru")
 
-fun main() {
-    println(user1.userData())
-    println(user2.userData())
+
+class User2 (
+    val uniqueIdentifier: Int,
+    val login: String,
+    var password: String,
+    val mail: String,
+    val bio: String = " "
+)
+{
+    fun userData() {
+        println("Данные пользователя: идентификационный номер: $uniqueIdentifier, логин: $login, " +
+                "пароль: $password , мейл: $mail, поле bio: $bio")
+
+    }
+    fun bioField() {
+        println("Введите текст о себе: ")
+        val fieldEntry = readln()
 }
+    fun changePassword() {
+        println("Введите текущий пароль: ")
+        val сurrentPassword = readln()
+        if (сurrentPassword == password) {
+            println("Введите новый пароль: ")
+            val newPassword = readln()
+            password = newPassword
+            println("Пароль изменен: $newPassword")
+        }
+    }
+}
+
+fun main() {
+    val user3 = User2(34567, "topol", "2L3G4K",
+    "topol44@mail.ru")
+
+    user3.bioField()
+    user3.changePassword()
+    user3.userData()
+}
+
+
