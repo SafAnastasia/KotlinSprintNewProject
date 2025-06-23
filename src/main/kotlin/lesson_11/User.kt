44091009d2b58046d4b58fab465c236bde1ec7ac
Skarler <black2244@inbox.ru>
2025-06-23 10:25:27 +0300

    KS-11-3 social network app

--- src/main/kotlin/lesson_11/User.kt
+++ src/main/kotlin/lesson_11/User.kt
@@ -1,58 +1,34 @@
 package org.example.lesson_11
 
 class User (
-    val uniqueIdentifier: Int,
-    val login: String,
-    val password: String,
-    val mail: String,
+    val userName: String,
+    val avatar: String,
+    val status: String = "микрофон выключен"
 )
-{
-fun userData() {
-    println("Данные пользователя: идентификационный номер: $uniqueIdentifier, логин: $login, " +
-            "пароль: $password , мейл: $mail")
-}
-}
 
-val user1 = User(123456, "kitis", "2G3K4L", "kitis32@mail.ru" )
-val user2 = User(67892, "cova", "9H8K4M", "sova22@mail.ru")
+class Room (
+    var cover: String,
+    var name: String,
+    var listParticipants: MutableList<User>
+) {
+    val participants = MutableList<User>()
 
-
-class User2 (
-    val uniqueIdentifier: Int,
-    val login: String,
-    var password: String,
-    val mail: String,
-    val bio: String = " "
-)
-{
-    fun userData() {
-        println("Данные пользователя: идентификационный номер: $uniqueIdentifier, логин: $login, " +
-                "пароль: $password , мейл: $mail, поле bio: $bio")
+    fun addingParticipant(user: User){
+        participants.add(user)
+        println("Добавить участника: ${user.userName} в комнату: $cover")
 
     }
-    fun bioField() {
-        println("Введите текст о себе: ")
-        val fieldEntry = readln()
-}
-    fun changePassword() {
-        println("Введите текущий пароль: ")
-        val сurrentPassword = readln()
-        if (сurrentPassword == password) {
-            println("Введите новый пароль: ")
-            val newPassword = readln()
-            password = newPassword
-            println("Пароль изменен: $newPassword")
-        }
+    fun statusUpdate(userName: String, newStatus: String) {
+        user.status = newStatus
+        println("Обновленный статус: $newStatus")
     }
 }
+
+val room = Room("обложка","разговоры")
+val user1 = User("anna","кот", "разговаривает")
+
 
 fun main() {
-    val user3 = User2(34567, "topol", "2L3G4K",
-    "topol44@mail.ru")
-
-    user3.bioField()
-    user3.changePassword()
-    user3.userData()
+    room.addingParticipant(user1)
+    room.statusUpdate("anna", "микрофон выключен")
 }
-
-
