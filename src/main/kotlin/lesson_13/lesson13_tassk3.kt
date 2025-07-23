4cbe52b03579dc772a232baa5dd561f8f6eb0052
skarlet22 <black2244@inbox.ru>
2025-07-23 04:55:24 -0700

    KS-13-3 edits made

--- src/main/kotlin/lesson_13/lesson13_tassk3.kt
+++ src/main/kotlin/lesson_13/lesson13_tassk3.kt
@@ -1,23 +1,24 @@
 package org.example.lesson_13
 
-class ContactDirectory (
-    var name: String,
+class PhoneBook(
+    val name: String,
     var number: Long,
     var company: String? = null
-) {
-    fun contactInformation () {
-        println("Имя: $name, Номер телефона: $number, Компания: ${company?: "<не указанно>"}")
-    }
-}
+)
+
 fun main() {
-    val contacts =  listOf (
-        ContactDirectory("Анна",89078906967, null),
-        ContactDirectory("Артем", 89065478987, null),
-        ContactDirectory("Алина", 89054783938, "null"),
-        ContactDirectory("Лиса", 89048906789, "Google"),
-        ContactDirectory("Дима", 89076785674, "Mail"))
-    println("Список в телефонной книге:")
-    contacts.mapNotNull { it.company }
-    contacts.filter { it.lowercase() != "null" }
-    contacts.forEach { println(it) }
+    val contacts = listOf(
+        PhoneBook("Анна", 89078906967, null),
+        PhoneBook("Артем", 89065478987, null),
+        PhoneBook("Алина", 89054783938, "null"),
+        PhoneBook("Лиса", 89048906789, "Google"),
+        PhoneBook("Дима", 89076785674, "Mail")
+    )
+
+    fun List<PhoneBook>.printInfo() {
+        for (contact in this) {
+            println(contact.company)
+        }
+    }
+    contacts.printInfo()
 }
