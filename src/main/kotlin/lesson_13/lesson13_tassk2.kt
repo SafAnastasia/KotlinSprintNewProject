21dd9a235b9c17365aa70fb8f79491cd25a3ffda
skarlet22 <black2244@inbox.ru>
2025-07-23 04:30:12 -0700

    KS-13-2 edits made

--- src/main/kotlin/lesson_13/lesson13_tassk2.kt
+++ src/main/kotlin/lesson_13/lesson13_tassk2.kt
@@ -1,17 +1,16 @@
 package org.example.lesson_13
 
-class TelDirectory (
-    var name: String,
-    var number: Long,
-    var company: String? = null
+class TelDirectory(
+    val name: String,
+    val number: Long,
+    val company: String? = null
 ) {
-    fun contactInformation () {
-        println("Имя: $name, Номер телефона: $number, Компания: ${company?: "<не указанно>"}")
+    fun printInfo() {
+        println("Имя: $name, Номер телефона: $number, Компания: ${company ?: "<не указанно>"}")
     }
 }
+
 fun main() {
-    val contact = TelDirectory("Анна",89078906967)
-    contact.contactInformation()
+    val contact = TelDirectory("Анна", 89078906967)
+    contact.printInfo()
 }
-
-
