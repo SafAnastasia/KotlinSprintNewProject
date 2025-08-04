9800d13ca25fa47075ed2cf95262ef6cc35d2f4e
skarlet22 <black2244@inbox.ru>
2025-08-04 19:38:42 +0300

    KS-14-1 edits have been made

--- src/main/kotlin/lesson_14/lesson14_tassk1.kt
+++ src/main/kotlin/lesson_14/lesson14_tassk1.kt
@@ -1,12 +1,51 @@
 package org.example.lesson_14
 
+open class Liner(
+    open val name: String,
+    open val speed: Int,
+    open val pasСapacity: Int,
+    open val cargoСapacity: Int
+) {
+    open fun info() {
+        println("Лайнер: скорость: $speed,\n количество пассажиров: $pasСapacity,\n вместимость грузов: $cargoСapacity")
+    }
+}
+
+class CargoShip(
+    override val name: String,
+    override val speed: Int,
+    override val pasСapacity: Int,
+    override val cargoСapacity: Int
+) : Liner(name, speed, pasСapacity, cargoСapacity) {
+
+    override fun info() {
+        println("Грузовое судно: скорость: $speed,\n количество пассажиров: $pasСapacity,\n вместимость грузов: $cargoСapacity")
+    }
+
+}
+
+class Icebreaker(
+    override val name: String,
+    override val speed: Int,
+    override val pasСapacity: Int,
+    override val cargoСapacity: Int
+) : Liner(name, speed, pasСapacity, cargoСapacity) {
+    override fun info() {
+        println("Ледокол: скорость: $speed,\n количество пассажиров: $pasСapacity,\n вместимость грузов: $cargoСapacity")
+    }
+
+    fun crushIce() {
+        println("Колет лед")
+    }
+}
+
 fun main() {
-    val liner = Liner()
-    val cargoShip = CargoShip()
-    val icebreaker = Icebreaker()
+    val liner = Liner("Anna", 30, 100, 1000)
+    val cargoShip = CargoShip("Ship", 20, 100, 2000)
+    val iceBreaker = Icebreaker("Ice", 15, 50, 1000)
 
     liner.info()
     cargoShip.info()
-    icebreaker.crushIce()
-    icebreaker.info()
+    iceBreaker.crushIce()
+    iceBreaker.info()
 }
