e721d7ec7a19f81bcd9d6b31c888953769f4e99e
skarlet22 <black2244@inbox.ru>
2025-08-15 17:20:01 +0300

    KS-14-3 class shape with two subclasses circle and rectangle

--- src/main/kotlin/lesson_14/lesson14_tassk3.kt
+++ src/main/kotlin/lesson_14/lesson14_tassk3.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_14
 
-import java.io.Writer
+import kotlin.math.PI
 
 abstract class Figure(
     val color: String
@@ -15,44 +15,46 @@ class Circle(
     val radius: Double
 ) : Figure(color) {
     override fun calculateArea(): Double {
-        return 3.14 * radius * radius
+        return PI * radius * radius
     }
 
     override fun calculatePerimeter(): Double {
-        return 2 * 3.14 * radius
+        return PI * 3.14 * radius
     }
 }
 
 class Rectangle(
     color: String,
-    val weight: Double,
+    val width: Double,
     val height: Double
 ) : Figure(color) {
     override fun calculateArea(): Double {
-        return weight * height
+        return width * height
     }
 
     override fun calculatePerimeter(): Double {
-        return 2 * (weight + height)
+        return 2 * (width + height)
     }
 }
 
 fun main() {
     val figure = listOf(
-        Circle("Black", 4.0),
-        Circle("White", 3.0),
-        Rectangle("Black", 4.0, 6.0),
-        Rectangle("White", 4.0, 3.0)
+        Circle(Black, 4.0),
+        Circle(White, 3.0),
+        Rectangle(Black, 4.0, 6.0),
+        Rectangle(White, 4.0, 3.0)
     )
-    val BlacPerimeterSum = figure
-        .filter { it.color == "Black" }
+    val blackPerimeterSum = figure
+        .filter { it.color == Black }
         .sumOf { it.calculatePerimeter() }
 
-    val WhiteAreaSum = figure
-        .filter { it.color == "White" }
+    val whiteAreaSum = figure
+        .filter { it.color == White }
         .sumOf { it.calculateArea() }
 
-    println("Сумма периметров всех черных фигур: %.2f".format(BlacPerimeterSum))
-    println("Сумма площадей всех белых фигур: %.2f".format(WhiteAreaSum))
+    println("Сумма периметров всех черных фигур: %.2f".format(blackPerimeterSum))
+    println("Сумма площадей всех белых фигур: %.2f".format(whiteAreaSum))
 }
 
+const val Black = "Black"
+const val White = "White"
