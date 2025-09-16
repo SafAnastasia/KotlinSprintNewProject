b960e9a02b5d36005189cb9ad4af461ff9c6a9b7
skarlet22 <black2244@inbox.ru>
2025-09-16 10:14:34 +0300

    KS-16-2-edits made

--- src/main/kotlin/lesson_16/lesson16_tassk2.kt
+++ src/main/kotlin/lesson_16/lesson16_tassk2.kt
@@ -1,21 +1,23 @@
 package org.example.lesson_16
 
+import kotlin.math.pow
+
 class Circle(private val radius: Double) {
     private companion object {
         const val PI = 3.14
     }
 
-    fun circumference(): Double {
+    fun getCircumference(): Double {
         return 2 * PI * radius
     }
 
     fun areaCircle(): Double {
-        return PI * radius * radius
+        return PI * radius.pow(2)
     }
 }
 
 fun main() {
     val circle = Circle(5.0)
-    println("Длина окружности: ${circle.circumference()}")
+    println("Длина окружности: ${circle.getCircumference()}")
     println("Площадь круга: ${circle.areaCircle()}")
 }
