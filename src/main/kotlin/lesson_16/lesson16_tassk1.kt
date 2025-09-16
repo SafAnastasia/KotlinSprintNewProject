1c594625e27dd3b04921da3fbb9d8d45fb7859b0
skarlet22 <black2244@inbox.ru>
2025-09-16 10:22:50 +0300

    KS-16-1-edits made

--- src/main/kotlin/lesson_16/lesson16_tassk1.kt
+++ src/main/kotlin/lesson_16/lesson16_tassk1.kt
@@ -3,13 +3,13 @@ package org.example.lesson_16
 import kotlin.random.Random
 
 class Cube {
-    private val number: Int = Random.nextInt(1, 7)
-    fun findNumber() {
+    private val number: Int = (1..6).random()
+    fun rollDice() {
         println("Выпало число: $number")
     }
 }
 
 fun main() {
     val cube = Cube()
-    cube.findNumber()
+    cube.rollDice()
 }
