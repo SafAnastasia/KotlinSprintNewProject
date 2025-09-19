1c9a66589e60d911913a61c52103ab1a297ccca0
skarlet22 <black2244@inbox.ru>
2025-09-19 09:53:49 +0300

    KS-17-3-edits have been made

--- src/main/kotlin/lesson_17/lesson17_tassk3.kt
+++ src/main/kotlin/lesson_17/lesson17_tassk3.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_17
 
-class Folder(private val realName: String, private val realNumberOfFiles: Int, private val isSecret: Boolean) {
-    val nameFolder: String
+class Folder(val realName: String, val realNumberOfFiles: Int, private val isSecret: Boolean) {
+    val folderName: String
         get() = if (isSecret) "скрытая папка" else realName
     val numberOfFiles: Int
         get() = if (isSecret) 0 else realNumberOfFiles
 }
 
-fun name() {
+fun main() {
     val secretFolder = Folder("Личная папка", 43, true)
 
-    println("Имя папки: ${secretFolder.nameFolder}")
+    println("Имя папки: ${secretFolder.folderName}")
     println("Количество файлов: ${secretFolder.numberOfFiles}")
 }
