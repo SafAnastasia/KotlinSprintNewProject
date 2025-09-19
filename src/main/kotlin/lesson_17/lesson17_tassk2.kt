cc47edf31e96bfb6d056c371fce181b10f450f46
skarlet22 <black2244@inbox.ru>
2025-09-19 09:39:57 +0300

    KS-17-2-edits have been made

--- src/main/kotlin/lesson_17/lesson17_tassk2.kt
+++ src/main/kotlin/lesson_17/lesson17_tassk2.kt
@@ -1,23 +1,19 @@
 package org.example.lesson_17
 
-class Ship ( private var name: String, var averageSpeed: Double, var homePort: Int) {
-    var name: String
-        get() = name
-        set(value) {
-            println("Имя коробря изменить нельзя")
+class Ship(name: String, var averageSpeed: Double, var homePort: String) {
+    var name: String = name
+        set(value) = println("Имя коробря изменить нельзя")
 
-        }
     fun infoShip() {
         println("Коробль: $name, скорость корабля: $averageSpeed, порт приписки: $homePort")
-
     }
 }
 
 fun main() {
-    val ship = Ship("Александра", 600, "Владивосток")
+    val ship = Ship("Александра", 600.0, "Владивосток")
     ship.infoShip()
     ship.name = "Аврора"
 
-    ship.averageSpeed = 234
+    ship.averageSpeed = 234.0
     ship.homePort = "Санкт-Петербург"
 }
