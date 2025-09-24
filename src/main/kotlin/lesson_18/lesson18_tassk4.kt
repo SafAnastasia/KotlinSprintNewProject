52a9621696deb999ee9fe0991a2f53c7d101012b
skarlet22 <black2244@inbox.ru>
2025-09-24 09:59:27 +0300

    KS-18-4-edits have been made

--- src/main/kotlin/lesson_18/lesson18_tassk4.kt
+++ src/main/kotlin/lesson_18/lesson18_tassk4.kt
@@ -1,31 +1,31 @@
 package org.example.lesson_18
 
 abstract class Box {
-    abstract fun surfaceArea(): Double
+    abstract fun getSurfaceArea(): Double
 }
 
-class rectangularBox(
-    val length: Double,
-    val width: Double,
-    val height: Double
+class RectangularBox(
+    private val length: Double,
+    private val width: Double,
+    private val height: Double
 ) : Box() {
-    override fun surfaceArea(): Double {
+    override fun getSurfaceArea(): Double {
         return 2 * (length * width + length * height + width * height)
     }
 }
 
-class cubicBox(
+class CubicBox(
     val ribLength: Double
 ) : Box() {
-    override fun surfaceArea(): Double {
+    override fun getSurfaceArea(): Double {
         return 6 * ribLength * ribLength
     }
 }
 
 fun main() {
-    val box1: Box = rectangularBox(10.0, 7.0, 3.0)
-    val box2: Box = cubicBox(3.0)
+    val box1: Box = RectangularBox(10.0, 7.0, 3.0)
+    val box2: Box = CubicBox(3.0)
 
-    println("Площадь поверхности прямоугольной посылки: ${box1.surfaceArea()}")
-    println("Площадь поверхности кубической посылки: ${box2.surfaceArea()}")
+    println("Площадь поверхности прямоугольной посылки: ${box1.getSurfaceArea()}")
+    println("Площадь поверхности кубической посылки: ${box2.getSurfaceArea()}")
 }
