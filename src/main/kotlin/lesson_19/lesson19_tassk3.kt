49ddfb4d6ac43926d8b42ad22aaa15eeac2e78de
skarlet22 <black2244@inbox.ru>
2025-09-26 13:04:57 +0300

    KS-19-3-spaceship class has been implemented

---
+++ src/main/kotlin/lesson_19/lesson19_tassk3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_19
+
+class SpiceShip() {
+
+    fun takeoff() {
+        //TODO: Здеьс нужна информация о дополнительной логике
+    }
+
+    fun land() {
+        throw NotImplementedError("Метод land() не реализован")
+    }
+
+    fun shootBackAsteroid() {
+        // реализация неизвестна
+    }
+}
